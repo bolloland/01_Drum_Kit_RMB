bd161566d43a0177a8e564cd01609127546d9821
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-03-28 13:40:59 -0500

    define key

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    console.log(e, audio)
+    // audio is now defined as an element on our page that has a data-key element of the ones we created on the html page
+
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    // now will find all elements with a KEY tag
+    // NOTICE to use .key and not key, as .key represents a class. 
+
+    console.log(key)
+
+
+    if (!audio) return
+    //if we hit a null button, dont return anything
+    audio.currentTime = 0 //rewinds sound file to beginning
+    audio.play()
+
+    key.classList.add('playing')
+    // same as: key.addClass('playing')
+    // we defined key above, as having those class attributes, and now we are adding a new classList attribute to it called ".playing", located in our css file. 
     })

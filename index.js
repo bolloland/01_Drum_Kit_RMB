69827111ee2520ed2b1f603e062896433fee65b2
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-03-28 14:22:50 -0500

    made playSound the callback function

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    // audio is now defined as an element on our page that has a data-key element of the ones we created on the html page
+const playSound = (e) => {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+        // audio is now defined as an element on our page that has a data-key element of the ones we created on the html page
+    
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+        // now will find all elements with a KEY tag
+        // NOTICE to use .key and not key, as .key represents a class. 
+    
+        if (!audio) return
+        //if we hit a null button, dont return anything
+        audio.currentTime = 0 //rewinds sound file to beginning
+        audio.play()
+    
+        key.classList.add('playing')
+        // same as: key.addClass('playing')
+        // we defined key above, as having those class attributes, and now we are adding a new classList attribute to it called ".playing", located in our css file. 
+    
+}
 
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-    // now will find all elements with a KEY tag
-    // NOTICE to use .key and not key, as .key represents a class. 
-
-    console.log(key)
-
-
-    if (!audio) return
-    //if we hit a null button, dont return anything
-    audio.currentTime = 0 //rewinds sound file to beginning
-    audio.play()
-
-    key.classList.add('playing')
-    // same as: key.addClass('playing')
-    // we defined key above, as having those class attributes, and now we are adding a new classList attribute to it called ".playing", located in our css file. 
-
+    window.addEventListener('keydown', playSound) 
     const removeTransition = (e) => {
-        console.log(e)
+        // console.log(e, e.propertyName)
+        if (e.propertyName != 'transform') return //skip if it's not transformed
+        // console.log(e.target)
+        e.target.classList.remove('playing')
     }
 
     //transition end event - identify all keys
     const keys = document.querySelectorAll('.key')
     // 'transitionend' is an event that's fired when a CSS transition is completed, callback is removeTrans which we then have to define...back up top. 
     keys.forEach(key => key.addEventListener('transitionend', removeTransition))
-
-    })
